0eeee7d9f25edd5d8b703883645383a0d4a5c2f1
Aranguren-Code <47396259+Aranguren-Code@users.noreply.github.com>
2021-05-03 17:02:02 -0500

    Add files via upload

---
+++ scenario_RCP_26_50/Hub_Spoke_P3_(model)(0).jl
@@ -0,0 +1,56 @@
+# Loading the necessary packages
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+using JuMP
+using Gurobi
+
+# Problem Description
+# Deterministic Hub and Spoke Network for Power Plants
+# ------------------------------
+
+#HS_P3=Model(solver=GurobiSolver(Presolve=0; OutputFlag=0))
+HS_P3=Model(solver=GurobiSolver(Presolve=-1, Cuts=0, MIPGap=0.001,
+  OutputFlag=0))
+
+# Variable Definition
+# ------------------------------
+
+@variable(HS_P3, DEPOTS[1:D], Bin)
+@variable(HS_P3, FLOWX[1:P, 1:D] >= 0)
+@variable(HS_P3, FLOWY[1:D, 1:C] >= 0)
+@variable(HS_P3, FLOWZ[1:P, 1:C] >= 0)
+@variable(HS_P3, SHORTAGE[1:C] >= 0)
+
+# Constraints Definition
+# ------------------------------
+
+# constraint to restrict supply of biomass
+
+@constraint(HS_P3, BIOMASS_SUPPLY[i=1:P], sum(FLOWX[i, j] for j=1:D) +
+  sum(FLOWZ[i, j] for j=1:C) <= Supply[i])
+
+# constraint for mass balance
+
+@constraint(HS_P3, MASS_BALANCE[j=1:D], sum(FLOWX[i, j] for i=1:P) ==
+  sum(FLOWY[j, i] for i=1:C))
+
+# constraint to assure supply of the demand
+
+@constraint(HS_P3, DEMAND_SUPPLY[j=1:C], sum(FLOWY[i, j] for i=1:D) +
+  sum(FLOWZ[i, j] for i=1:P) + SHORTAGE[j] == Demand[j])
+
+# constraint for biomass storage at depots
+
+@constraint(HS_P3, BIOMASS_PROCESS[j=1:D], sum(FLOWX[i, j] for i=1:P) <=
+  DepotCap[j]*DEPOTS[j])
+
+# Objective Setting
+# ------------------------------
+
+@objective(HS_P3, Min, sum((HarvCost[i] + TransCost1[i, j]) *
+  FLOWX[i, j] for i=1:P, j=1:D) +
+  sum((TransCost2[i, j]) * FLOWY[i, j] for i=1:D, j=1:C) +
+  sum((HarvCost[i] + TransCost3[i, j]) * FLOWZ[i, j] for i=1:P, j=1:C) +
+  sum(DepotCost[i]*DEPOTS[i] for i=1:D) +
+  sum(ShortCost[i]*SHORTAGE[i] for i=1:C))
